a4fabc2670c23625fd35f9f76bfa365312f4f5c5
eip <eip@users.noreply.github.com>
2016-04-22 15:50:40 +0300

    Pack addon files to zip script

---
+++ .wrk/pack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+cd ../../
+#SCRIPT_PATH=`pwd`;
+#echo ${SCRIPT_PATH}
+ls -la
+rm webinterface.ktalk*.zip 2> /dev/null
+rm webinterface.ktalk/webinterface.ktalk*.zip 2> /dev/null
+zip -r webinterface.ktalk.zip webinterface.ktalk/* -x "*/.*" "*/*.app/*" "*/*.zip" "*/screenshot.*"
+ADDON_VER=`cat webinterface.ktalk/addon.xml | grep "^\s*version=" | grep -o "[0-9\.]\+"`
+mv webinterface.ktalk.zip webinterface.ktalk/webinterface.ktalk-${ADDON_VER}.zip
+popd  > /dev/null
