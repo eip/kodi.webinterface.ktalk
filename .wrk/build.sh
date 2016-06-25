c6226488c2bf2bb8c30154c3eb1a0828933b8973
eip <eip@users.noreply.github.com>
2016-06-25 19:17:18 +0300

    Shell scripts linted with shellcheck.

--- .wrk/build.sh
+++ .wrk/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 SCRIPT_PATH="${BASH_SOURCE[0]}";
 if ([ -h "${SCRIPT_PATH}" ]) then
-  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=$(readlink "${SCRIPT_PATH}"); done
 fi
 pushd . > /dev/null
-cd `dirname ${SCRIPT_PATH}` > /dev/null
-cd ../../
+cd "$(dirname "${SCRIPT_PATH}")" > /dev/null || exit
+cd ../../ || exit
 
 # Building plugin distributive
 rm plugin.webinterface.ktalk*.zip 2> /dev/null
 rm webinterface.ktalk/plugin.webinterface.ktalk*.zip 2> /dev/null
 zip -r plugin.webinterface.ktalk.zip webinterface.ktalk/* -x "*/.*" "*/*.app/*" "*/*.zip" "*/README.*" "*/LICENSE" "*/screenshot*.*" "*/*.sublime*" "*/test/*" "*/*.map" "*/js/framework7.js*"
-ADDON_VER=`cat webinterface.ktalk/addon.xml | grep "^<addon\s.*\sversion=" | grep -o "[0-9][0-9\.]\+"`
-mv plugin.webinterface.ktalk.zip webinterface.ktalk/plugin.webinterface.ktalk-${ADDON_VER}.zip
+ADDON_VER=$(< webinterface.ktalk/addon.xml grep "^<addon\s.*\sversion=" | grep -o "[0-9][0-9\.]\+")
+mv plugin.webinterface.ktalk.zip webinterface.ktalk/plugin.webinterface.ktalk-"${ADDON_VER}".zip
 
 # Update repository addon data
 ADDON_DIR=repository.eip/addons/plugin.webinterface.ktalk
@@ -21,15 +21,15 @@ mkdir ${ADDON_DIR}
 cp webinterface.ktalk/addon.xml ${ADDON_DIR}
 cp webinterface.ktalk/changelog.txt ${ADDON_DIR}
 cp webinterface.ktalk/icon.png ${ADDON_DIR}
-cp webinterface.ktalk/plugin.webinterface.ktalk-${ADDON_VER}.zip ${ADDON_DIR}
+cp webinterface.ktalk/plugin.webinterface.ktalk-"${ADDON_VER}".zip ${ADDON_DIR}
 
 # Update repository info (addons.xml)
 rm repository.eip/addons/addons.xml 2> /dev/null
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" > repository.eip/addons/addons-tmp.xml
 echo "<addons>" >> repository.eip/addons/addons-tmp.xml
-find repository.eip/addons -name addon.xml | sort | while read f;
+find repository.eip/addons -name addon.xml | sort | while read -r f;
 do
-  cat "$f" | grep --invert-match "^<?xml" >> repository.eip/addons/addons-tmp.xml
+  < "${f}" grep --invert-match "^<?xml" >> repository.eip/addons/addons-tmp.xml
 done;
 echo "</addons>" >> repository.eip/addons/addons-tmp.xml
 xmllint --format --encode UTF-8 repository.eip/addons/addons-tmp.xml > repository.eip/addons/addons.xml
--- .wrk/get-resources.sh
+++ .wrk/get-resources.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SCRIPT_PATH="${BASH_SOURCE[0]}";
 if ([ -h "${SCRIPT_PATH}" ]) then
-  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=$(readlink "${SCRIPT_PATH}"); done
 fi
-SCRIPT_PATH=`dirname ${SCRIPT_PATH}` 
+SCRIPT_PATH="$(dirname "${SCRIPT_PATH}")"
 JS_PATH=$SCRIPT_PATH"/../js"
 #curl https://raw.githubusercontent.com/github/fetch/master/fetch.js -o ${JS_PATH}"/fetch.js"
-curl https://raw.githubusercontent.com/nolimits4web/Framework7/master/dist/js/framework7.min.js -o ${JS_PATH}"/framework7.min.js"
-curl https://raw.githubusercontent.com/nolimits4web/Framework7/master/dist/js/framework7.min.js.map -o ${JS_PATH}"/framework7.min.js.map"
+curl https://raw.githubusercontent.com/nolimits4web/Framework7/master/dist/js/framework7.min.js -o "${JS_PATH}""/framework7.min.js"
+curl https://raw.githubusercontent.com/nolimits4web/Framework7/master/dist/js/framework7.min.js.map -o "${JS_PATH}""/framework7.min.js.map"
